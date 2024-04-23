1326d4060379dbb3931740f70313aae603384296
Ivan Setya <75768911+ivansetya@users.noreply.github.com>
2024-04-23 18:37:04 +0700

    Delete bread_collection.py

--- bread_collection.py
+++
@@ -1,7 +0,0 @@
-warehouse = {
-    0 : {"name" : "Whole Wheat Bread", "stock" : 10, "price" : 40000, "promo" : True},
-    1 : {"name" : "Sourdough Bread", "stock" : 15, "price" : 45000, "promo" : True},
-    2 : {"name" : "Cranberry Cheese Bread", "stock" : 5, "price" : 50000, "promo" : False},
-    3 : {"name" : "Choco Bread", "stock" : 10, "price" : 25000, "promo" : False}
-}
-
